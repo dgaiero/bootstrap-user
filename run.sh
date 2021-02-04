51442493d8b0065886b3a29313c6ec8880b6607a
Dominic Gaiero <dpgaiero@gmail.com>
2021-02-04 14:59:17 -0800

    update printf

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 if [  ! -f $HOME/.profile_config ]; then
    cd /var/userbootstrap
-   printf "--------------------------------------------------------------\n"
+   printf -- "--------------------------------------------------------------\n"
    printf "Starting user configuration script. This could take some time.\n"
-   printf "--------------------------------------------------------------\n"
+   printf -- "--------------------------------------------------------------\n"
    pipenv install --ignore-pipfile &> /dev/null
    if [ $? -eq 0 ]; then
       pipenv run python bootstrap.py
@@ -15,15 +15,15 @@ if [  ! -f $HOME/.profile_config ]; then
    printf "Cleaning up\n"
    pipenv --rm &> /dev/null
    if [ $? -eq 0 ]; then
-      printf "----------------------------------------------------------------\n"
+      printf -- "----------------------------------------------------------------\n"
       printf "Your account is setup correctly.\nPlease logout to finish setup.\n"
       printf "(You can use the logout command)\n\n"
-      printf "----------------------------------------------------------------\n"
+      printf -- "----------------------------------------------------------------\n"
       touch $HOME/.profile_config
    else
-      printf "--------------------------------------\n"
+      printf -- "--------------------------------------\n"
       printf "The setup script encountered an error.\n"
       printf "I'll try again next time you login.\n"
-      printf "--------------------------------------\n"
+      printf -- "--------------------------------------\n"
    fi
 fi
